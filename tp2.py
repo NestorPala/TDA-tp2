932a5b3b01c0e7a426db83f8a419cae0c34b75fa
NestorPala <npalavecino@fi.uba.ar>
2024-05-06 03:00:45 -0300

    agregado descripcion ecuacion de recurrencia

--- tp2.py
+++ tp2.py
@@ -18,16 +18,16 @@ def tp2_dp(lista_xi, lista_fj):
     OPT = inicializar_memo(n)
     enemigos_eliminados = 0
 
-    for minuto_destino in range(1, n+1):
+    for minuto_actual in range(1, n+1):
         for minuto_origen in range(n):
 
-            if minuto_destino <= minuto_origen:
+            if minuto_actual <= minuto_origen:
                 continue
 
             if minuto_origen == 0:
                 no_usar_opt = True
 
-            minuto_destino_ = minuto_destino - 1
+            minuto_actual_ = minuto_actual - 1
 
             if no_usar_opt:
                 maximo_batallas_anteriores = 0
@@ -35,11 +35,15 @@ def tp2_dp(lista_xi, lista_fj):
             else:
                 maximo_batallas_anteriores = max(OPT[minuto_origen - 1][:])
 
-            j = minuto_destino_ - minuto_origen
-            ataque_actual = min(lista_xi[minuto_destino_], lista_fj[j])
+            j = minuto_actual_ - minuto_origen
+            ataque_actual = min(lista_xi[minuto_actual_], lista_fj[j])
 
-            OPT[minuto_destino_][minuto_origen] = maximo_batallas_anteriores + ataque_actual
-            
+            # ecuacion de recurrencia: OPT[i][j] = max(OPT[i-1][k] ∀ k ∈ {0,...,j-1}) + min(X[i], f(j))
+            # X = lista de cantidad enemigos en el minuto i ("lista_xi")
+            # f = función de recarga ("lista_fj")
+
+            OPT[minuto_actual_][minuto_origen] = maximo_batallas_anteriores + ataque_actual
+
     print(OPT)
 
     return enemigos_eliminados, OPT
@@ -60,7 +64,6 @@ def main():
     # x = [254,515, 647, 454, 126, 406,  69,  48, 781, 920]
     # f = [170,312,1000,2131,2975,3026,3035,3402,3463,3496]
 
-    #  enemigos_eliminados = 2118, ataques = [647,454,406,69,920]
     enemigos_eliminados, orden_recargar_atacar = tp2(x, f)
 
     print("Enemigos eliminados: ", enemigos_eliminados)
